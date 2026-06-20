0f0e25b9c2b18c27d2174b3c28087353b5460dc9
HemeshKanyal <hemeshkanyal1@gmail.com>
2026-06-21 00:59:24 +0530

    trying to debug ci cd pipeline

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,11 @@
 import { prismaClient } from "db/client";
 
 export default async function Home() {
-  const users = await prismaClient.user.findMany();
+  //const users = await prismaClient.user.findMany();
   return (
     <div>
-      {JSON.stringify(users)}
+      {/* {JSON.stringify(users)} */}
+      Home page
     </div>
   );
 }
